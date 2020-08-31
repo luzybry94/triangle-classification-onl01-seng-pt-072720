8cb81c525987fe8b31226525e03acd4127e9c95b
Chaim Bryski <luzybry94@gmail.com>
2020-08-31 18:58:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,16 @@ class Triangle
   end
   
   def kind
-    if @a <= 0 || @b <= 0 || @c <= 0 
+    if @a + @b < @c || @a + @c < @b || @b + @c < @a || @a <= 0 || @b <= 0 || @c <= 0
+      raise TriangleError
+    elsif @a == @b && @b == @c
+      return :equilateral
+    elsif @a != @b && @b != @c && @a != @c
+      return :scalene
+    else 
+      return :isosceles
+    end
+  end
   
   class TriangleError < StandardError
   

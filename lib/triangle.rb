14fe1500abdab2c182a5950f5321032829315fb8
Chaim Bryski <luzybry94@gmail.com>
2020-08-31 18:43:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Triangle
   # write code here
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    if @a + @b < @c || @a + @c < @b || @b + @c < @a && @a <= 0 && @b <= 0 && @c <= 0
+      raise TriangleError
+    elsif @a == @b && @b == @c
+      return :equilateral
+    elsif @a != @b && @b != @c && @a != @c
+      return :scalene
+    else 
+      return :isosceles
+    end
+  end
+  
+  class TriangleError < StandardError
+  
+  end
+  
 end
